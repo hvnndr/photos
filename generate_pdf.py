871cc8f1bad0ad3497bf45be047e5ac0156fc00d
Hevannder Rubian <74639120+hvnndr@users.noreply.github.com>
2024-01-01 00:03:03 -0400

    Update generate_pdf.py

--- generate_pdf.py
+++ generate_pdf.py
@@ -3,29 +3,41 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from reportlab.lib.utils import ImageReader
-from PIL import Image
+from PIL import Image, ImageOps
+from ironpdf import *
+
 
 
 def get_image_files_list(image_folder_path):
     return [f for f in os.listdir(image_folder_path)
-                   if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+            if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+
+
+def break_page(wh, ih, sbi, ds):
+    if wh - (ih + sbi + ds) <= 0:
+        return True
+    return False
 
 
 def create_pdf(image_folder_path, output_filename):
     canvas_dimensions = A4
-    canvas_heigth = canvas_dimensions[1]
-    canvas_width = canvas_dimensions[0]
     font_size = 8
     margin = 0.2 * inch
     space_between_images = 0.2 * inch
+    images_per_line = 6
+    space_between_lines = 0.5 * inch
+    quality = 10
+    description_space = 0.2 * inch
+
+    canvas_heigth = canvas_dimensions[1]
+    canvas_width = canvas_dimensions[0]
     workspace_width = canvas_width - 2 * margin
-    each_image_space = (workspace_width - 4 * space_between_images) / 5
+    workspace_height = canvas_heigth - 2 * margin
+    each_image_space = (workspace_width - (images_per_line - 1) * space_between_images) / images_per_line
     image_height = each_image_space
     image_width = each_image_space
-    x_position = margin
     y_position = canvas_dimensions[1] - margin - image_height
-    space_between_lines = 0.5 * inch
-
+    x_position = margin
 
     c = canvas.Canvas(output_filename, pagesize=canvas_dimensions)
     c.setTitle("amostras")
@@ -37,36 +49,50 @@ def create_pdf(image_folder_path, output_filename):
         if index == 0:
             y_position = canvas_dimensions[1] - margin - image_height
         else:
-            if index % 5 == 0:
+            if index % images_per_line == 0:
                 y_position -= image_height + space_between_lines
                 x_position = margin
+                if y_position < 0 or workspace_height - (y_position + image_height + space_between_images) <= 0:
+                    c.showPage()
+                    c.setFont("Helvetica", font_size)
+                    y_position = canvas_dimensions[1] - margin - image_height
             else:
                 x_position += image_width + space_between_images
 
         img_path = os.path.join(image_folder_path, image_file_name)
         original_image = Image.open(img_path)
-        original_image.save('./compressed.jpg', quality=5)
+        original_image = ImageOps.exif_transpose(original_image)
+
+        original_image.save('./compressed.jpg', quality=quality, )
         img = ImageReader('./compressed.jpg')
-        c.drawImage(img, x_position, y_position, width=image_width, height=image_height, preserveAspectRatio=True, anchor='s')
+        c.drawImage(img, x_position, y_position, width=image_width, height=image_height, preserveAspectRatio=True,
+                    anchor='s')
 
         image_caption = f'{image_file_name}'
         image_caption_width = c.stringWidth(image_caption)
         text_x_position = x_position + ((image_width - image_caption_width) / 2)
-        text_y_position = y_position - 0.2 * inch
+        text_y_position = y_position - description_space
         c.drawString(text_x_position, text_y_position, image_caption)
         print(f'OK: {image_file_name}')
 
     c.save()
 
 
-create_pdf(
-    image_folder_path='fotos',
-    output_filename='uncompressed.pdf'
-)
+def compress_pdf():
+    pdf = PdfDocument("normal.pdf")
+    pdf.CompressImages(50)
+    pdf.SaveAs("normal.pdf")
+
+# create_pdf(
+#     image_folder_path='normal',
+#     output_filename='normal.pdf'
+# )
+
+compress_pdf()
 
 
-
-# determinar quantas fotos por linha
-# diminuir a qualidade da imagem
 # ou diminuir a qualidade do pdf
-#criar tmp e deletar
+# criar tmp e deletar
+# reescrever codigo mais estruturado com POO
+# mostrar porcentagem gerada no pdf
+# mostrar porcentagem de compressao
